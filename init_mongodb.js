4823574eb8536ba6ea2f3b144372ed20be7e742a
Tim Bradley <fennasnogothrim@gmail.com>
2016-02-20 10:24:47 -0700

    Added configuration checks and associated messages; simplified async usage.

--- init_mongodb.js
+++ init_mongodb.js
@@ -11,10 +11,13 @@ const COLLECTION_SIZE_FACTOR_ERRORLOG = .0002;
 const COLLECTION_SIZE_FACTOR_SOCKETDATA = .0998;
 
 const ERROR_MESSAGE_INDEX = 'There was an error creating the index %s on the collection "%s".';
-const ERROR_MESSAGE_CAPPED = 'There was an error creating the capped collection "%s" with the size [%s bytes].';
-
 const SUCCESS_MESSAGE_INDEX = 'Successfully created the index %s on the collection "%s".';
-const SUCCESS_MESSAGE_CAPPED = 'Successfully created the capped collection "%s" with the size [%s bytes].';
+
+const ERROR_MESSAGE_CAPPED = 'There was an error creating the capped collection "%s" with the size [%d bytes].';
+const SUCCESS_MESSAGE_CAPPED = 'Successfully created the capped collection "%s" with the size [%d bytes].';
+const SKIPPED_MESSAGE_CAPPED = 'Skipped creating a capped collection "%s".';
+
+const ERROR_MISSING_CONFIG = 'The configuration value %s is missing; it\'s required to %s.';
 
 // Get the configuration
 var configPath = path.join(__dirname, "config.json");
@@ -24,30 +27,51 @@ var conString = config.conString;
 var MongoClient = mongodb.MongoClient;
 
 function withMongoConnection(callback) {
-  MongoClient.connect(conString, function(err, db) {
-    if (err) console.error('error fetching client from pool', err);
-    callback(err, db);
-  });
+  if(config.conString) {
+    MongoClient.connect(config.conString, function(err, db) {
+      if (err) console.error('error fetching client from pool', err);
+      callback(err, db);
+    });
+  } else {
+      var errorMessage = sprintf(ERROR_MISSING_CONFIG, 'conString', 'connect to the database');
+      console.log(errorMessage);
+
+      var error = new Error(errorMessage);
+      callback(error, {});
+  }
 }
 
 function createCappedCollection(collection, size, callback) {
-  withMongoConnection(function(err, db){
-    if(err) callback(err, null);
-    db.createCollection(collection, {
-        capped:true,
-        size: size
-      }, function(err, result){
+  if(config.cap_collections) {
+    if(config.cap_total_size) {
+      withMongoConnection(function(err, db){
+        if(err) return callback(err, null);
+        db.createCollection(collection, {
+            capped:true,
+            size: size
+          }, function(err, result){
 
-        if(err) {
-          console.error(sprintf(ERROR_MESSAGE_CAPPED, collection, size), err);
-        } else {
-          console.log(sprintf(SUCCESS_MESSAGE_CAPPED, collection, size));
-        }
+            if(err) {
+              console.error(sprintf(ERROR_MESSAGE_CAPPED, collection, size), err);
+            } else {
+              console.log(sprintf(SUCCESS_MESSAGE_CAPPED, collection, size));
+            }
 
-        callback(err, { err:err, result:result });
-        db.close();
+            callback(err, { err:err, result:result });
+            db.close();
+          });
       });
-  });
+    } else {
+      var errorMessage = sprintf(ERROR_MISSING_CONFIG, 'cap_total_size', 'create capped collections');
+      console.log(errorMessage);
+
+      var error = new Error(errorMessage);
+      callback(error, {});
+    }
+  } else {
+    console.log(sprintf(SKIPPED_MESSAGE_CAPPED, collection));
+    callback(null, {});
+  }
 }
 
 function createCollectionIndex(collection, index, callback) {
@@ -68,42 +92,43 @@ function createCollectionIndex(collection, index, callback) {
   });
 }
 
-var dbChanges = {};
-if(config.cap_collections) {
-  dbChanges.wikiedits_cap = function(callback) {
+var dbChanges = [
+  function(callback) {
     var size = (config.cap_total_size * COLLECTION_SIZE_FACTOR_WIKIEDITS);
     var collection = 'wikiedits';
     createCappedCollection(collection, size, callback);
-  };
-  dbChanges.errorlog_cap = function(callback) {
+  },
+
+  function(callback) {
     var size = (config.cap_total_size * COLLECTION_SIZE_FACTOR_ERRORLOG);
     var collection = 'errorlog';
     createCappedCollection(collection, size, callback);
-  };
-  dbChanges.socketdata_cap = function(callback) {
+  },
+
+  function(callback) {
     var size = (config.cap_total_size * COLLECTION_SIZE_FACTOR_SOCKETDATA);
     var collection = 'socketdata';
     createCappedCollection(collection, size, callback);
-  };
-}
+  },
 
-dbChanges.socketdata_new_revision_index = function(callback) {
-  var collection = 'socketdata';
-  var index = {'message.revision.new': 1, 'message.wiki': 1};
-  createCollectionIndex(collection, index, callback);
-};
+  function(callback) {
+    var collection = 'socketdata';
+    var index = {'message.revision.new': 1, 'message.wiki': 1};
+    createCollectionIndex(collection, index, callback);
+  },
 
-dbChanges.socketdata_new_revision_and_wiki_index = function(callback) {
-  var collection = 'wikiedits';
-  var index = {'revnew': 1, 'wiki': 1};
-  createCollectionIndex(collection, index, callback);
-};
+  function(callback) {
+    var collection = 'wikiedits';
+    var index = {'revnew': 1, 'wiki': 1};
+    createCollectionIndex(collection, index, callback);
+  },
 
-dbChanges.socketdata_title_and_wiki_index = function(callback) {
-  var collection = 'wikiedits';
-  var index = {'title': 1, 'wiki': 1};
-  createCollectionIndex(collection, index, callback);
-};
+  function(callback) {
+    var collection = 'wikiedits';
+    var index = {'title': 1, 'wiki': 1};
+    createCollectionIndex(collection, index, callback);
+  }
+];
 
 // Do all the DB initialization queries in parallel
 async.parallel(dbChanges, function(err, resultList) {
