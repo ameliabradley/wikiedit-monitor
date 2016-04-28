fc7f88e052b554ea5f4a11be22d291c94116d788
Timothy Michael Bradley <fennasnogothrim@gmail.com>
2016-04-27 23:22:51 -0700

    Updated MongoConnector to reuse connection, per MongoDB/Node driver recommendations.

--- src/Analytics/AssociationTracker.js
+++ src/Analytics/AssociationTracker.js
@@ -63,9 +63,7 @@ class AssociationTracker {
                                 }
                             },
                             { upsert: true },
-                            function(){
-                                db.close();
-                            }
+                            function(){}
                     );
                 });
             });
