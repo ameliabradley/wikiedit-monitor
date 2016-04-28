8568292358f834163556268b91e779dd78624fdd
Timothy Michael Bradley <fennasnogothrim@gmail.com>
2016-04-27 23:22:51 -0700

    Updated MongoConnector to reuse connection, per MongoDB/Node driver recommendations.

--- src/EditLog/EditLog.js
+++ src/EditLog/EditLog.js
@@ -194,7 +194,7 @@ function EditLog (config, m_eventEmitter) {
         if (oRev && oRev.title === strTitle) {
            deletedRevisions.push(oRev);
            logError(revid, "Revision could not be queried because article was deleted", oRev, "", false);
-        m_revisionList.purgeRevision(revid);
+           m_revisionList.purgeRevision(revid);
            iRevsDeleted++;
         }
      }
