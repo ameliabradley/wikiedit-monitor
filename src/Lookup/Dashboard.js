695e7cc39bc5ad6061a79d8649474ecf8274bd14
Timothy Michael Bradley <fennasnogothrim@gmail.com>
2016-04-27 20:34:57 -0700

    Added "Most Active Articles" graph to dashboard.

--- src/Lookup/Dashboard.js
+++ src/Lookup/Dashboard.js
@@ -8,4 +8,23 @@ function dashboardView(cxt, callback){
   }
 }
 
+dashboardView.ajaxCall = function(cxt, callback, errorCallback){
+  if(cxt.url.query.dash_socketdata) {
+    var wiki = cxt.wiki;
+    var db = cxt.db;
+    db.collection('socketdata_last_hour')
+        .find({'message.wiki': wiki + 'wiki'})
+        .toArray(function(error, data){
+            db.close();
+            if(error) errorCallback(error);
+            callback({
+              wiki: wiki,
+              data: data
+            });
+        });
+    return true;
+  }
+  return false;
+}
+
 module.exports = dashboardView;
