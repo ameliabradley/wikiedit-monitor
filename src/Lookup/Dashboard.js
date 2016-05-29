dc135f917d528a2ae0812a2cea469231df3b14e1
Timothy Michael Bradley <fennasnogothrim@gmail.com>
2016-05-29 01:06:03 -0700

    Dashboard now uses timestamp-indexed socketdata collection for queries.

--- src/Lookup/Dashboard.js
+++ src/Lookup/Dashboard.js
@@ -9,19 +9,32 @@ function dashboardView(cxt, callback) {
 
 dashboardView.ajaxCall = function(cxt, callback, errorCallback) {
   if (cxt.url.query.dash_socketdata) {
-    var wiki = cxt.wiki;
-    var db = cxt.db;
-    db.collection('socketdata_last_hour')
-      .find({
-        'message.wiki': wiki + 'wiki'
-      })
-      .toArray(function(error, data) {
-        if (error) errorCallback(error);
-        callback({
-          wiki: wiki,
-          data: data
-        });
-      });
+    if(
+        cxt.url.query.time_start
+        && cxt.url.query.time_end
+    ) {
+        var wiki = cxt.wiki;
+        var db = cxt.db;
+        db.collection('socketdata')
+          .find({
+            'message.timestamp': {
+                $lt: parseInt(cxt.url.query.time_end),
+                $gte: parseInt(cxt.url.query.time_start)
+            },
+            'message.wiki': wiki + 'wiki'
+          })
+          .toArray(function(error, data) {
+            if (error) errorCallback(error);
+            callback(null, {
+              wiki: wiki,
+              data: data
+            });
+          });
+    } else {
+        var err = new Error('time_start and time_end query parameters are required');
+        callback(err, undefined, 422);
+        return true;
+    }
     return true;
   }
   return false;
