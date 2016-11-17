0a93517a3038568f812cab7f0f280436b4ec5d5d
Tim Bradley <fennasnogothrim@gmail.com>
2016-11-17 01:02:20 -0700

    Added ability to completely discard and renew socket connection.

--- src/EditLog/SocketManager.js
+++ src/EditLog/SocketManager.js
@@ -4,77 +4,111 @@ var io = require('primus-socket.io-client'),
 function SocketManager() {
   var self = this;
 
+  const DEFAULT_MAX_RECONNECTION_ATTEMPTS = 13;
+  const RECONNECT_TIMEOUT_MS = 120000; // 2 Minutes
+
   var m_socket;
-  var m_shouldSubscribe = true;
+  var m_connectFunction;
 
+  self.reconnect = function(){
+    m_socket.disconnect();
+    m_socket = undefined;
+    delete require.cache[require.resolve('primus-socket.io-client')]
+    io = require('primus-socket.io-client');
+    m_connectFunction();
+  };
   self.connect = function(config, changeCallback, errorCallback) {
-    const DEFAULT_MAX_RECONNECTION_ATTEMPTS = 13;
 
-    // Requires socket.io-client 0.9.x:
-    // browser code can load a minified Socket.IO JavaScript library;
-    // standalone code can install via 'npm install socket.io-client@0.9.1'.
-    m_socket = io.connect('stream.wikimedia.org/rc', {
-      query: 'hidebots=1',
-      'max reconnection attempts': config.max_reconnection_attempts || DEFAULT_MAX_RECONNECTION_ATTEMPTS
-    });
-
-    var subscribeToStream = debounce(function() {
-      if (m_shouldSubscribe) {
-        m_socket.emit('subscribe', 'en.wikipedia.org');
-        m_shouldSubscribe = false;
+    m_connectFunction = function(){
+      // Requires socket.io-client 0.9.x:
+      // browser code can load a minified Socket.IO JavaScript library;
+      // standalone code can install via 'npm install socket.io-client@0.9.1'.
+      m_socket = io.connect('stream.wikimedia.org/rc', {
+        query: 'hidebots=1',
+        'max reconnection attempts': config.max_reconnection_attempts || DEFAULT_MAX_RECONNECTION_ATTEMPTS
+      });
+      var shouldSubscribe = true;
+      var reconnectTimeout = setTimeout(self.reconnect, RECONNECT_TIMEOUT_MS);
+      function bumpTime(){
+        if(typeof reconnectTimeout !== undefined) {
+	  clearTimeout(reconnectTimeout);
+	  reconnectTimeout = setTimeout(function(){
+	    reconnectTimeout = undefined;
+            self.reconnect()
+          }, RECONNECT_TIMEOUT_MS);
+        }
       }
-    }, 1000);
 
-    m_socket.on('connect', function() {
-      console.log('***** CONNECTED to stream.wikimedia.org/rc');
-      subscribeToStream();
-    });
+      var subscribeToStream = debounce(function() {
+        if (shouldSubscribe) {
+          m_socket.emit('subscribe', 'en.wikipedia.org');
+          shouldSubscribe = false;
+        }
+      }, 1000);
 
-    m_socket.on('change', function(message) {
-      m_shouldSubscribe = false;
-      changeCallback(message);
-    });
+      m_socket.on('connect', function() {
+        bumpTime();
+        console.log('***** CONNECTED to stream.wikimedia.org/rc');
+        subscribeToStream();
+      });
 
-    m_socket.on('disconnect', function() {
-      console.log('***** Socket Disconnected');
-      m_shouldSubscribe = true;
-    });
+      m_socket.on('change', function(message) {
+        bumpTime();
+        shouldSubscribe = false;
+        changeCallback(message);
+      });
 
-    m_socket.on('connect_error', function(err) {
-      console.log('***** Socket Connection Error', err);
-      errorCallback('socket connect event error', err);
-    });
+      m_socket.on('disconnect', function() {
+        bumpTime();
+        console.log('***** Socket Disconnected');
+        shouldSubscribe = true;
+      });
 
-    m_socket.on('reconnect_error', function(err) {
-      console.log('***** Socket Reconnection Error', err);
-      errorCallback('socket reconnect event error', err);
-    });
+      m_socket.on('connect_error', function(err) {
+        bumpTime();
+        console.log('***** Socket Connection Error', err);
+        errorCallback('socket connect event error', err);
+      });
 
-    m_socket.on('reconnect_failed', function() {
-      console.log('***** Socket Reconnection Failed');
-    });
+      m_socket.on('reconnect_error', function(err) {
+        bumpTime();
+        console.log('***** Socket Reconnection Error', err);
+        errorCallback('socket reconnect event error', err);
+      });
 
-    m_socket.on('connect_timeout', function() {
-      console.log('***** Socket Connection Timeout');
-    });
+      m_socket.on('reconnect_failed', function() {
+        bumpTime();
+        console.log('***** Socket Reconnection Failed');
+      });
 
-    m_socket.on('reconnect', function(attemptNumber) {
-      console.log('***** Socket Reconnect ', attemptNumber);
-    });
+      m_socket.on('connect_timeout', function() {
+        bumpTime();
+        console.log('***** Socket Connection Timeout');
+      });
 
-    m_socket.on('reconnect_attempt', function() {
-      console.log('***** Socket Reconnection Attempt');
-    });
+      m_socket.on('reconnect', function(attemptNumber) {
+        bumpTime();
+        console.log('***** Socket Reconnect ', attemptNumber);
+      });
 
-    m_socket.on('reconnecting', function(attemptNumber) {
-      console.log('***** Socket Reconnecting...', attemptNumber);
-    });
+      m_socket.on('reconnect_attempt', function() {
+        bumpTime();
+        console.log('***** Socket Reconnection Attempt');
+      });
+
+      m_socket.on('reconnecting', function(attemptNumber) {
+        bumpTime();
+        console.log('***** Socket Reconnecting...', attemptNumber);
+      });
+    };
+    m_connectFunction();
   };
 
   self.disconnect = function() {
-    // There seems to be literally no way of shutting this bad boy down
-    //m_socket.removeAllListeners();
-    //m_socket.disconnect();
+    m_socket.disconnect();
+    m_socket = undefined;
+    delete require.cache[require.resolve('primus-socket.io-client')]
+    io = require('primus-socket.io-client');
   };
 }
 
